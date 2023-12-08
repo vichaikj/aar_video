84d29696ed82c63f6b4adbb56364997c98a0267b
Jean Philippe <jean-philippe.vichaikit@hotmail.com>
2023-12-09 00:53:45 +0100

    make fetch topic function

--- src/process/fetch_reddit.py
+++ src/process/fetch_reddit.py
@@ -45,41 +45,47 @@ story_path = os.path.join(script_dir, "../resources/story.txt")
 _LIMIT = 1
 # Init number, to not modify
 _STORY_NUMBER = 0
-for submission in top_posts:
-    # Check if the post is from the last month
-    title = submission.title.lower()
-    if submission.created_utc > one_month_ago_timestamp:
-        if any(keyword in title and title not in done_videos for keyword in story_keywords):
-            # Fetch and filter comments for each post
-            submission.comments.replace_more(limit=0)
-            comment_list = submission.comments.list()
-            filtered_comments = [
-                comment
-                for comment in comment_list
-                if len(comment.body) > 700 and comment.depth == 0 and comment.score > 5000
-            ]
 
-            if filtered_comments:
-                # Sort comments by score in descending order
-                sorted_comments = sorted(filtered_comments, key=lambda x: x.score, reverse=True)
 
-                # Regex to remove patterns such as "&#x200B;"
-                pattern = re.compile(r"&#[xX]?(?P<code>[0-9a-fA-F]+);")
+def fetch_topic(limit=_LIMIT, story_number=_STORY_NUMBER):
+    for submission in top_posts:
+        # Check if the post is from the last month
+        title = submission.title.lower()
+        if submission.created_utc > one_month_ago_timestamp:
+            if any(keyword in title and title not in done_videos for keyword in story_keywords):
+                # Fetch and filter comments for each post
+                submission.comments.replace_more(limit=0)
+                comment_list = submission.comments.list()
+                filtered_comments = [
+                    comment
+                    for comment in comment_list
+                    if len(comment.body) > 700 and comment.depth == 0 and comment.score > 5000
+                ]
 
-                # Get the top comment information
-                top_comment = sorted_comments[0]
-                print(f"Title: {submission.title}")
-                print(f"URL: {submission.url}")
-                print(f"Top Comment score: {top_comment.score}")
-                print(f"Top Comment: \n{pattern.sub('', unescape(top_comment.body))}")
+                if filtered_comments:
+                    # Sort comments by score in descending order
+                    sorted_comments = sorted(filtered_comments, key=lambda x: x.score, reverse=True)
 
-                with open(story_path, "w") as out_file:
-                    out_file.write(submission.title + "\n" + pattern.sub('', unescape(top_comment.body)))
+                    # Regex to remove patterns such as "&#x200B;"
+                    pattern = re.compile(r"&#[xX]?(?P<code>[0-9a-fA-F]+);")
 
-                _STORY_NUMBER += 1
-                if _STORY_NUMBER >= _LIMIT:
-                    break
-            else:
-                print("No comments above 700 characters.")
+                    # Get the top comment information
+                    top_comment = sorted_comments[0]
+                    print(f"Title: {submission.title}")
+                    print(f"URL: {submission.url}")
+                    print(f"Top Comment score: {top_comment.score}")
+                    print(f"Top Comment: \n{pattern.sub('', unescape(top_comment.body))}")
 
-            print("\n" + "-"*50 + "\n")
+                    with open(story_path, "w") as out_file:
+                        out_file.write(submission.title + "\n" + pattern.sub('', unescape(top_comment.body)))
+
+                    story_number += 1
+                    if story_number >= limit:
+                        break
+                else:
+                    print("No comments above 700 characters.")
+
+                print("\n" + "-"*50 + "\n")
+
+
+fetch_topic()
